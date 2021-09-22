3944772685b6af3a938125dcfb45c225eaab29b3
Vipin Kumar <vipinkmboj20@gmail.com>
2021-09-22 13:06:20 +0530

    changesss

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -992,10 +992,11 @@ var cartItemsModel = require('../modules/cartitemsschema');
 var purchasedModel = require('../modules/purchasedschema');
 const { error } = require('console');
 //
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY_Test
-//const stripeSecretKey = process.env.STRIPE_SECRET_KEY_Live 
+//const stripeSecretKey = process.env.STRIPE_SECRET_KEY_Test
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY_Live 
 
 //const stripePublicKey = process.env.STRIPE_PUBLIC_KEY_Test//process.env.STRIPE_PUBLIC_KEY_Live;
+//const stripePublicKey = process.env.STRIPE_PUBLIC_KEY_Live;
 const stripe = require('stripe')(stripeSecretKey);
 
 
